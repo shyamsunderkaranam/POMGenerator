309d37cee04a22680eabb949335de5ff86e4c5a5
shyamsunderkaranam <40713356+shyamsunderkaranam@users.noreply.github.com>
2025-01-10 17:38:53 +0530

    Create App3.jsx

---
+++ src/com/pomgenerators/App3.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+
+const Table = ({ data }) => {
+  const excludeFields = ['fieldToExclude1', 'fieldToExclude2'];
+
+  const filteredData = data.map(item => {
+    const filteredItem = { ...item };
+    excludeFields.forEach(field => delete filteredItem[field]);
+    return filteredItem;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(filteredData[0]).map((key, index) => (
+            <th key={index}>{key}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {filteredData.map((item, index) => (
+          <tr key={index} onClick={() => showModal(item)}>
+            {Object.values(item).map((value, index) => (
+              <td key={index}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
+
+************************
+import React, { useState } from 'react';
+import Table from './Table';
+import Modal from './Modal';
+import data from './data.json'; // Replace with your JSON data
+
+const App = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const showModal = (item) => {
+    setSelectedItem(item);
+  };
+
+  const closeModal = () => {
+    setSelectedItem(null);
+  };
+
+  return (
+    <div>
+      <Table data={data} showModal={showModal} />
+      {selectedItem && <Modal item={selectedItem} closeModal={closeModal} />}
+    </div>
+  );
+};
+
+export default App;
+***************
+const Modal = ({ item, closeModal }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={closeModal}>&times;</span>
+        <h2>Item Details</h2>
+        <pre>{JSON.stringify(item, null, 2)}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
+*********
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
