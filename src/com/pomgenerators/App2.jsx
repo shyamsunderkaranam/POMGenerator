5dad5d863ee2e456f1a8375a539aa672e353e936
shyamsunderkaranam <40713356+shyamsunderkaranam@users.noreply.github.com>
2025-03-08 21:33:07 +0530

    Actual changes

--- src/com/pomgenerators/App2.jsx
+++ src/com/pomgenerators/App2.jsx
@@ -3,10 +3,21 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import Pagination from '@mui/material/Pagination';
+import Button from '@mui/material/Button';
+import LaunchIcon from '@mui/icons-material/Launch';
 import axios from 'axios';
+import './pagination.css';
+import './App.css';
+import { Typography } from '@mui/material';
 
-const ITEMS_PER_PAGE = 5;
+const ITEMS_PER_PAGE = 20;
 
 const EnvironmentTable = ({ data, searchQuery, currentPage, setPageCount }) => {
   const [personas, setPersonas] = useState([]);
@@ -33,28 +44,39 @@ const EnvironmentTable = ({ data, searchQuery, currentPage, setPageCount }) => {
     [filteredFeatures, currentPage]);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Feature</th>
-            {personas.map((persona) => (
-              <th key={persona}>{persona}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {displayFeatures.map((feature) => (
-            <tr key={feature}>
-              <td>{feature}</td>
-              {personas.map((persona) => (
-                <td key={persona}>{data[persona].ftvalues[feature]}</td>
-              ))}
-            </tr>
+    <TableContainer sx={{ maxHeight: 440 }}>
+    <Table stickyHeader aria-label="sticky table">
+      <TableHead >
+        <TableRow className="table-header">
+          <TableCell>
+            <Typography>Feature Toggle Name</Typography>
+            </TableCell>
+          {personas.map((persona) => (
+            <TableCell key={persona}>
+              <Typography>{persona}</Typography>
+            </TableCell>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {displayFeatures.map((feature) => (
+          <TableRow key={feature}>
+            <TableCell>
+            <Typography>{feature}</Typography>
+            </TableCell>
+            {personas.map((persona) => (
+              <TableCell key={persona}>
+                {/* <a className="ftvalue" href={data[persona]?.url+"."+feature} target="_blank" > */}
+                <Button href={data[persona]?.url+"."+feature}  target="_blank" variant="contained" endIcon={<LaunchIcon />}>
+                  {data[persona]?.ftvalues[feature] ?? "Not Applicable"}
+                </Button>
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+    </TableContainer>
   );
 };
 
@@ -64,32 +86,48 @@ const TabPanel = ({ value, index, children }) => (
   </div>
 );
 
-function App() {
+function App2() {
   const [data, setData] = useState({});
   const [tier, setTier] = useState(0);
   const [env, setEnv] = useState(0);
   const [searchQuery, setSearchQuery] = useState('');
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const [pageCount, setPageCount] = useState(0);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('/api/data')
-      .then(response => setData(response.data))
-      .catch(error => console.error("Error fetching data: ", error));
+    setLoading(true);
+    setError(false);
+    axios.get('https://url.com')
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+        setError(false);
+        console.log(response.data);
+        })
+      .catch(error => {
+        console.error("Error fetching data: ", error);
+        setLoading(false);
+        setError(true);
+        });
   }, []);
-
-  const tiers = Object.keys(data);
-  const environments = tiers.length > 0 ? Object.keys(data[tiers[tier]]) : [];
 
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
   };
 
+  const tiers = Object.keys(data);
+  const environments = tiers.length > 0 ? Object.keys(data[tiers[tier]]) : [];
+
   return (
     <div className="App">
-      <Tabs 
-        value={tier} 
-        onChange={(e, newValue) => { setTier(newValue); setEnv(0); setCurrentPage(1); setSearchQuery(''); }}
+        <h2> Feature Toggles Dashboard</h2>
+        {(loading)&& <h3>Loading the data. Please wait....</h3> }
+      {(!loading && !error && data) && 
+      <>
+      <Tabs value={tier} 
+        onChange={(e, newValue) => { setTier(newValue); setEnv(0); setCurrentPage(0); setSearchQuery(''); }}
         variant="scrollable"
         scrollButtons="auto"
       >
@@ -99,12 +137,11 @@ function App() {
       </Tabs>
       {tiers.map((tierName, tierIndex) => (
         <TabPanel value={tier} index={tierIndex} key={tierIndex}>
-          <Tabs 
-            value={env} 
-            onChange={(e, newValue) => { setEnv(newValue); setCurrentPage(1); setSearchQuery(''); }}
+          <Tabs value={env} 
+            onChange={(e, newValue) => { setEnv(newValue); setCurrentPage(0); setSearchQuery(''); }}
             variant="scrollable"
             scrollButtons="auto"
-          >
+            >
             {environments.map((envName, index) => (
               <Tab label={envName} key={index} />
             ))}
@@ -122,9 +159,10 @@ function App() {
               <EnvironmentTable 
                 data={data[tierName][envName]} 
                 searchQuery={searchQuery}
-                currentPage={currentPage - 1}
+                currentPage={currentPage}
                 setPageCount={setPageCount}
               />
+              
               <Pagination
                 count={pageCount}
                 page={currentPage}
@@ -132,13 +170,16 @@ function App() {
                 color="primary"
                 showFirstButton
                 showLastButton
+
               />
             </TabPanel>
           ))}
         </TabPanel>
       ))}
+      </>}
+      {(error)&& <h3>Error while getting the data. Check browsers console for more info..... </h3> }
     </div>
   );
 }
 
-export default App;
+export default App2;
